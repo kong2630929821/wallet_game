28427fb3b0e03e56eaef129606e0506ed6ecac61
hezl <772545428@qq.com>
2018-12-19 20:33:26 +0800

    FAIR-1251 挖矿：搭建配置 根据权重获得奖品 添加矿山

--- src/earn/server/rpc/user_item.r.ts
+++ src/earn/server/rpc/user_item.r.ts
@@ -1,20 +1,36 @@
 /**
  * 
  */
+import { DEFAULT_FILE_WARE } from '../../../pi_pt/constant';
 import { getEnv } from '../../../pi_pt/net/rpc_server';
 import { Bucket } from '../../utils/db';
-import { BTC, ETH, Hoe, Item, Items, KT, Mine, ST } from '../data/db/item.s';
-import { items_init } from '../util/item_util.r';
+import { BTC, ETH, Hoe, Item, Items, KT, Mine, Prizes, ST } from '../data/db/item.s';
+import { add_itemCount, get_mine_total, get_mine_type, items_init } from '../util/item_util.r';
 import { ItemQuery } from './itemQuery.s';
+
+// 添加矿山
+// #[rpc=rpcServer]
+export const add_mine = (uid: number): Mine => {
+    // if (get_mine_total(uid) >= 9) return;
+    const itemQuery = new ItemQuery();
+    itemQuery.uid = uid;
+    itemQuery.enumType = 1;
+    itemQuery.itemType = get_mine_type();
+    const item = add_itemCount(itemQuery, 1);
+
+    return <Mine>item.value;
+};
 
 // 查询指定用户物品信息
 // #[rpc=rpcServer]
 export const item_query = (uid: number): Items => {
     console.log('item query in !!!!!!!!!!!!');
     const dbMgr = getEnv().getDbMgr();
-    const userItemBucket = new Bucket('file', Items._$info.name, dbMgr);
+    const userItemBucket = new Bucket(DEFAULT_FILE_WARE, Items._$info.name, dbMgr);
     const items = <Items>userItemBucket.get(uid)[0];
-    if (!items) return;
+    if (!items) {
+        items_init(uid);
+    }
 
     return items;
 };
@@ -35,3 +51,12 @@ export const get_item = (itemQuery: ItemQuery): Item => {
         }
     }
 };
+
+// 查询用户获取奖品信息
+// #[rpc=rpcServer]
+// export const award_query = (uid: number): Prizes => {
+//     const dbMgr = getEnv().getDbMgr();
+//     const userPrizeBucket = new Bucket(DEFAULT_FILE_WARE, Prizes._$info.name, dbMgr);
+//     const iter =  userPrizeBucket.iter(1);
+    
+// }
