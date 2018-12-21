d3e1e52503e90057f100377560e7fceb1726c30e
luob <luob1215@gmail.com>
2018-12-21 11:00:59 +0800

    # 封装oauth调用，和兑换邀请码

---
+++ src/earn/server/util/http_client.ts
@@ -0,0 +1,45 @@
+import { Atom } from '../../../pi_pt/rust/atom';
+import { HttpClient } from '../../../pi_pt/rust/httpc';
+import { createHttpClient, HttpClientBody, HttpClientOptions, postString } from '../../../pi_pt/rust/pi_serv/js_httpc';
+
+/**
+ * 创建客户端
+ */
+export const createClient = () => {
+    return createHttpClient(HttpClientOptions.default());
+};
+
+/**
+ * 添加头信息
+ * 'content-type':'application/json'
+ */
+export const addHeader = (client: HttpClient, key: string, value: string) => {
+    HttpClient.addHeaderSharedHttpc(client, Atom.fromFrom(key), Atom.fromFrom(value));
+};
+
+/**
+ * jsonrpc
+ * @param url -localhost + port
+ * @param param -body
+ */
+export const post = (client: HttpClient, url: string, param: JSON) => {
+    console.log('!!!!!!!!!!param:', param, JSON.stringify(param));
+    const body = HttpClientBody.bodyString(JSON.stringify(param));
+    const result = { ok: null, err: null };
+    try {
+        const r = postString(client, Atom.fromFrom(url), body);
+        if (((typeof r[1] === 'string' && (<any>r[1]).endsWith('Result is Err')) || (typeof r[1].self === 'string' && (<any>r[1].self).endsWith('Result is Err')))) {
+            result.err = r[1];
+
+            return result;
+        } else {
+            result.ok = r[1].text();
+
+            return result;
+        }
+    } catch (e) {
+        result.err = e;
+
+        return result;
+    }
+};
