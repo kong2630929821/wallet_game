0960fc86ffec67f67be2eef1884c6ecdcb427ba7
hezl <772545428@qq.com>
2019-01-03 16:54:51 +0800

    FAIR-1310 添加ST兑换商城，从钱包账户扣除相应货币 删除奖券活动

--- src/earn/server/rpc/stParties.r.ts
+++ src/earn/server/rpc/stParties.r.ts
@@ -4,10 +4,12 @@
 
 import { randomInt } from '../../../pi/util/math';
 import { getEnv } from '../../../pi_pt/net/rpc_server';
+import { DBIter } from '../../../pi_pt/rust/pi_serv/js_db';
 import { Bucket } from '../../utils/db';
-import { AWARD_SRC_ROTARY, GOLD_TICKET_ROTARY, LEVEL1_ROTARY_STCOST, LEVEL2_ROTARY_STCOST, LEVEL3_ROTARY_STCOST, RAINBOW_TICKET_ROTARY, RESULT_SUCCESS, SILVER_TICKET_ROTARY, ST_TYPE, ST_UNIT_NUM, ST_WALLET_TYPE, WALLET_API_QUERY, WARE_NAME } from '../data/constant';
-import { AwardResponse, FreeRotary } from '../data/db/item.s';
-import { DB_ERROR, REQUEST_WALLET_FAIL, ROTARY_TYPE_ERROR, ST_NOT_ENOUGH } from '../data/errorNum';
+import { STConvertCfg } from '../../xlsx/awardCfg.s';
+import { AWARD_SRC_CONVERT, AWARD_SRC_ROTARY, AWARD_SRC_TREASUREBOX, LEVEL1_ROTARY_AWARD, LEVEL1_ROTARY_STCOST, LEVEL1_TREASUREBOX_AWARD, LEVEL1_TREASUREBOX_STCOST, LEVEL2_ROTARY_AWARD, LEVEL2_ROTARY_STCOST, LEVEL2_TREASUREBOX_AWARD, LEVEL2_TREASUREBOX_STCOST, LEVEL3_ROTARY_AWARD, LEVEL3_ROTARY_STCOST, LEVEL3_TREASUREBOX_AWARD, LEVEL3_TREASUREBOX_STCOST, MEMORY_NAME, RESULT_SUCCESS, ST_TYPE, ST_UNIT_NUM, ST_WALLET_TYPE, WALLET_API_QUERY, WARE_NAME } from '../data/constant';
+import { AwardResponse, ConvertTab, FreeRotary } from '../data/db/item.s';
+import { AWARD_NOT_ENOUGH, DB_ERROR, REQUEST_WALLET_FAIL, ROTARY_TYPE_ERROR, ST_NOT_ENOUGH } from '../data/errorNum';
 import { doAward } from '../util/award.t';
 import { add_award, add_itemCount, reduce_itemCount } from '../util/item_util.r';
 import { oauth_send } from '../util/oauth_lib';
@@ -29,7 +31,7 @@ export const get_STNum = (): CoinQueryRes => {
         const json = JSON.parse(r.ok);
         if (json.return_code === 1) {
             // 根据平台数据库存储的单位进行转换
-            const walletST = json.balance * ST_UNIT_NUM;
+            const walletST = json.balance / ST_UNIT_NUM;
             coinQueryRes.num = walletST;
             console.log('http success walletST!!!!!!!!!!!!!!!!!!!!', json.balance);
         } else {
@@ -52,7 +54,7 @@ export const st_rotary = (rotaryType:number): AwardResponse => {
     let stCount;
     let hasfree = false;
     switch (rotaryType) {
-        case SILVER_TICKET_ROTARY:
+        case LEVEL1_ROTARY_AWARD:
             stCount = LEVEL1_ROTARY_STCOST;
             // 获取是否还有免费的初级转盘次数
             const dbMgr = getEnv().getDbMgr();
@@ -63,10 +65,10 @@ export const st_rotary = (rotaryType:number): AwardResponse => {
             freeRotary.free = false;
             bucket.put(uid, freeRotary);
             break;
-        case GOLD_TICKET_ROTARY:
+        case LEVEL2_ROTARY_AWARD:
             stCount = LEVEL2_ROTARY_STCOST;
             break;
-        case RAINBOW_TICKET_ROTARY:
+        case LEVEL3_ROTARY_AWARD:
             stCount = LEVEL3_ROTARY_STCOST;
             break;
         default:
@@ -74,6 +76,7 @@ export const st_rotary = (rotaryType:number): AwardResponse => {
 
             return awardResponse;
     }
+    console.log('stCount:!!!!!!!!!!!!!!!', stCount);
     // 如果有免费次数使用免费次数
     if (hasfree === true) {
         const v = [];
@@ -91,7 +94,7 @@ export const st_rotary = (rotaryType:number): AwardResponse => {
         awardResponse.award = award;
 
         return awardResponse;
-    } else if (!reduce_itemCount(ST_TYPE, stCount)) { // 扣除相应ST
+    } else if (!reduce_itemCount(ST_TYPE, stCount)) { // 没有免费次数扣除相应ST
         awardResponse.resultNum = ST_NOT_ENOUGH;
 
         return awardResponse;
@@ -119,7 +122,23 @@ export const st_treasurebox = (treasureboxType:number): AwardResponse => {
     const uid = getUid();
     const randomMgr = new RandomSeedMgr(randomInt(1, 10000));
     const awardResponse = new AwardResponse();
-    if (!reduce_itemCount(ST_TYPE, LEVEL1_ROTARY_STCOST)) {
+    let stCount;
+    switch (treasureboxType) {
+        case LEVEL1_TREASUREBOX_AWARD:
+            stCount = LEVEL1_TREASUREBOX_STCOST;
+            break;
+        case LEVEL2_TREASUREBOX_AWARD:
+            stCount = LEVEL2_TREASUREBOX_STCOST;
+            break;
+        case LEVEL3_TREASUREBOX_AWARD:
+            stCount = LEVEL3_TREASUREBOX_STCOST;
+            break;
+        default:
+            awardResponse.resultNum = ROTARY_TYPE_ERROR;
+
+            return awardResponse;
+    }
+    if (!reduce_itemCount(ST_TYPE, stCount)) {
         awardResponse.resultNum = ST_NOT_ENOUGH;
 
         return awardResponse;
@@ -129,7 +148,7 @@ export const st_treasurebox = (treasureboxType:number): AwardResponse => {
     const count = v[0][1];
     const newitemType = v[0][0];
     add_itemCount(uid, newitemType, count);
-    const award =  add_award(uid, newitemType, count, AWARD_SRC_ROTARY);
+    const award =  add_award(uid, newitemType, count, AWARD_SRC_TREASUREBOX);
     if (!award) {
         awardResponse.resultNum = DB_ERROR;
 
@@ -138,6 +157,61 @@ export const st_treasurebox = (treasureboxType:number): AwardResponse => {
     awardResponse.resultNum = RESULT_SUCCESS;
     awardResponse.award = award;
 
+    return awardResponse;
+};
+
+// ST兑换
+// #[rpc=rpcServer]
+export const st_convert = (awardType:number):AwardResponse => {
+    const uid = getUid();
+    const awardResponse = new AwardResponse();
+    const dbMgr = getEnv().getDbMgr(); 
+    const bucket = new Bucket(MEMORY_NAME, STConvertCfg._$info.name, dbMgr);
+    const convertCfg = bucket.get<number, [STConvertCfg]>(awardType)[0];
+    // 从配置中获取具体兑换信息
+    if (!convertCfg) {
+        awardResponse.resultNum = DB_ERROR;
+
+        return awardResponse;
+    }
+    const stCount = convertCfg.count;
+    const desc = convertCfg.desc;
+    const convertBucket = new Bucket(WARE_NAME, ConvertTab._$info.name, dbMgr);
+    // 从数据库获取兑换码
+    const iter = <DBIter>convertBucket.iter(null);
+    let convertAward:ConvertTab;
+    do {
+        const iterConvert = iter.nextElem();
+        console.log('elCfg----------------read---------------', iterConvert);
+        if (!iterConvert) {
+            awardResponse.resultNum = AWARD_NOT_ENOUGH;
+            
+            return awardResponse;
+        }
+        const convertTab:ConvertTab = iterConvert[1];
+        if ((convertTab.typeNum === awardType) && (convertTab.state === true)) {
+            convertAward = convertTab;
+            break;
+        }
+    } while (iter);
+    // 扣除相应ST
+    if (!reduce_itemCount(ST_TYPE, stCount)) {
+        awardResponse.resultNum = ST_NOT_ENOUGH;
+        
+        return awardResponse;
+    }
+    const award = add_award(uid, awardType, convertCfg.num, AWARD_SRC_CONVERT, convertAward.convert, desc);
+    if (!award) {
+        awardResponse.resultNum = DB_ERROR;
+
+        return awardResponse;
+    }
+    // 已发出的兑换码数据库状态改为false
+    convertAward.state = false;
+    convertBucket.put(convertAward.id, convertAward);
+    awardResponse.award = award;
+    awardResponse.resultNum = RESULT_SUCCESS;
+    
     return awardResponse;
 };
 
